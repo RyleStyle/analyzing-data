6874c5c440b5e8fcbd8ef20484f58ba444f195fe
Ryan Smith <ryantsmith2023@gmail.com>
2022-08-18 13:52:53 -0700

    QOL changes, no longer hard-coded

--- work.js
+++ work.js
@@ -1,9 +1,24 @@
+/*
+How much do you make per second? Want to track that for every second you work?
+Look no further. Please follow the instructions below.
+*/
+
+
+let hourlyRate = 18;
+// FILL YOUR OWN HOURLY RATE IN ABOVE. (only the number)
+// Example: let hourlyRate = 15;
+
+
+// ------DO NOT CHANGE ANYTHING PAST THIS POINT------
+
 let money = 0;
 let time = 0;
+
+let moneyPerSec = hourlyRate/60/60
 setInterval(() => {
     
     // $0.005 is how much you make per second if you make $18 an hour.
-    money = money + 0.005
+    money = money + moneyPerSec
     console.log(money)
 
 
