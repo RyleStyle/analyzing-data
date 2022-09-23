71bbe34390c276242d78b02bbf06bdc74ec71fb5
Ryan Smith <ryantsmith2023@gmail.com>
2022-09-23 01:36:29 -0700

    split.js

---
+++ split.js
@@ -0,0 +1,12 @@
+const string = "id,name"
+
+string.split(",")
+
+console.log(string)
+console.log(string.split(","))
+
+let stringOne = string.split(",")[0] // id
+let stringTwo = string.split(",")[1] // name
+
+console.log(stringOne)
+console.log(stringTwo)
